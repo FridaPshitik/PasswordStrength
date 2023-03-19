7c73eb2fd03a46863759c224c1a3f7649cda1a9e
Frida Pshitik <FridaP@skyvar.co.il>
2023-03-19 15:05:54 +0200

    Reverting to the original code works fine

--- password-strength/app.js
+++ password-strength/app.js
@@ -1,58 +1,74 @@
 
-class AbstractBaseRule extends Error{
-    _handle(password) {
-        if (this.check(password)) {
-           return true;
+function PasswordStrength(password) {
+
+    return isLowerCharacter(password)
+        && isUpperCharacter(password)
+        && isDigit(password)
+        && isSpecialCharacter(password)
+        && isLength(password)
+        && isRepeatMoreThanTwice(password)
+        && isSequencesOfThree(password)
+
+}
+
+function isLowerCharacter(password) {
+
+    for (let i = 0; i < password.length; i++) {
+        if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122) return true;
+    }
+    return false;
+}
+
+function isUpperCharacter(password) {
+
+    for (let i = 0; i < password.length; i++) {
+        if (password.charCodeAt(i) >= 65 && password.charCodeAt(i) <= 90) return true;
+    }
+    return false;
+}
+
+function isDigit(password) {
+
+    for (let i = 0; i < password.length; i++) {
+        if (password.charCodeAt(i) >= 49 && password.charCodeAt(i) <= 57) return true;
+    }
+    return false;
+}
+
+function isSpecialCharacter(password) {
+
+    for (let i = 0; i < password.length; i++) {
+        if (password.charCodeAt(i) >= 33 && password.charCodeAt(i) <= 45
+            || password.charCodeAt(i) == 63 || password.charCodeAt(i) == 64) return true;
+    }
+    return false;
+}
+
+function isLength(password) {
+
+    return password.length >= 8;
+}
+
+function isRepeatMoreThanTwice(password) {
+
+    let mone = 0;
+
+    for (let i = 0; i < password.length - 1; i++) {
+        mone = 0;
+        for (let j = i + 1; j < password.length; j++) {
+            if (password[i] == password[j]) mone++;
         }
+        if (mone >= 2) return false;
     }
-
-    check(password) {
-        throw new Error('Abstract non-implemented method');
-    }
+    return true;
 }
 
-class HasLowerCase extends AbstractBaseRule {
-    check(password) {
-        return /[a-z]/.test(password)
+function isSequencesOfThree(password) {
+
+    for (let i = 0; i < password.length - 2; i++) {
+        if (password.charCodeAt(i) == password.charCodeAt(i + 1) - 1 && password.charCodeAt(i) == password.charCodeAt(i + 2) - 2) return false;
     }
+    return true;
 }
 
-class HasUpperCase extends AbstractBaseRule {
-    check(password) {
-        return /[A-Z]/.test(password)
-    }
-}
-
-class HasDigit extends AbstractBaseRule {
-    check(password) {
-        let pass = /\d/
-        return pass.test(password)
-    }
-}
-
-class HasLength extends AbstractBaseRule {
-    check(password) {
-        return password.length>=8;
-    }
-}
-
-class HasSpecialCharacter extends AbstractBaseRule {
-    check(password) {
-        let pass = /[@$!%*?&]/
-        return pass.test(password)
-    }
-}
-
-class HasRepeatMoreThanTwice extends AbstractBaseRule {
-    check(password) {
-        let pass = /([a-zA-Z0-9])\1{2,}/
-        return pass.test(password)
-    }
-}
-
-class HasSequencesOfThree extends AbstractBaseRule {
-    check(password) {
-        let pass=/([a-zA-Z0-9]{3})/
-        return pass.test(password)
-    }
-}
+console.log(PasswordStrength("ab34@"));
